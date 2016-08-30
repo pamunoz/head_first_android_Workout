004ff19dfe3fa9753259f8b8831b6fdcf86a13d9
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-30 18:34:01 -0300

    Add the fragment transactions

--- app/src/main/java/com/pfariasmunoz/workout/MainActivity.java
+++ app/src/main/java/com/pfariasmunoz/workout/MainActivity.java
@@ -2,6 +2,7 @@ package com.pfariasmunoz.workout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.FragmentTransaction;
 
 public class MainActivity extends Activity
                           implements WorkoutListFragment.WorkoutListListener {
@@ -14,6 +15,18 @@ public class MainActivity extends Activity
 
     @Override
     public void itemClicked(long id) {
-        // the code to set the detail will go here
+        // the code to set the detail
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        // start the fragment transaction
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        // replace the fragment and add it to the back stack
+        ft.replace(R.id.fragment_container, details);
+        // add it to the back stack
+        ft.addToBackStack(null);
+        // get the new and old fragments to face in and out
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        // commit the transaction
+        ft.commit();
     }
 }
