a3e90ae5f9eb5ac8740bb8ad664697a193a0530b
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-29 23:34:46 -0300

    Add the list of workouts to the WorkoutListFragment

--- app/src/main/java/com/pfariasmunoz/workout/WorkoutListFragment.java
+++ app/src/main/java/com/pfariasmunoz/workout/WorkoutListFragment.java
@@ -5,6 +5,7 @@ import android.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 public class WorkoutListFragment extends ListFragment {
 
@@ -12,6 +13,18 @@ public class WorkoutListFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] names = new String[Workout.workouts.length];
+        for (int i = 0; i < names.length; i++) {
+            // creates an String array of the workouts names
+            names[i] = Workout.workouts[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(), android.R.layout.simple_expandable_list_item_1,
+                names);
+        // bind the array adapter to the list view
+        setListAdapter(adapter);
+
         // calling the superclass onCreateView() method give the default layout for the ListFragment
         return super.onCreateView(inflater, container, savedInstanceState);
     }
