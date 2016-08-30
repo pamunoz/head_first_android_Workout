90bd9276d3fbfc77b62afa13a4ca4811a02b74cc
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-30 16:29:32 -0300

    Make the activity implements the interface

--- app/src/main/java/com/pfariasmunoz/workout/MainActivity.java
+++ app/src/main/java/com/pfariasmunoz/workout/MainActivity.java
@@ -3,16 +3,17 @@ package com.pfariasmunoz.workout;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+                          implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorkoutDetailFragment frag = (WorkoutDetailFragment)getFragmentManager().findFragmentById(R.id.detail_frag);
-        // This is a setter method for the workout ID
-        // The activity will use this method to set the value of the workout ID.
-        // This is to get WorkoutDetailFragment to display detail of workout hereto check it's working
-        frag.setWorkout(1);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        // the code to set the detail will go here
     }
 }
