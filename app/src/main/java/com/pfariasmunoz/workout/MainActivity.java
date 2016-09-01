a60506b241e8b66518a4c026dc9c9369acaa3de1
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-01 16:58:53 -0300

    Add selection of layout depending on the device

--- app/src/main/java/com/pfariasmunoz/workout/MainActivity.java
+++ app/src/main/java/com/pfariasmunoz/workout/MainActivity.java
@@ -1,8 +1,11 @@
 package com.pfariasmunoz.workout;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends Activity
                           implements WorkoutListFragment.WorkoutListListener {
@@ -15,18 +18,31 @@ public class MainActivity extends Activity
 
     @Override
     public void itemClicked(long id) {
-        // the code to set the detail
-        WorkoutDetailFragment details = new WorkoutDetailFragment();
-        // start the fragment transaction
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        details.setWorkout(id);
-        // replace the fragment and add it to the back stack
-        ft.replace(R.id.fragment_container, details);
-        // add it to the back stack
-        ft.addToBackStack(null);
-        // get the new and old fragments to face in and out
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        // commit the transaction
-        ft.commit();
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        // We only need to run the code if the fragment layout is there
+        // meaning: if the app is in a tablet (using the layout from layout-large directory)
+        if (fragmentContainer != null) {
+            // the code to set the detail
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            // start the fragment transaction
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            // replace the fragment and add it to the back stack
+            ft.replace(R.id.fragment_container, details);
+            // add it to the back stack
+            ft.addToBackStack(null);
+            // get the new and old fragments to face in and out
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            // commit the transaction
+            ft.commit();
+        } else {
+            // if the frame layout isn't there, the app must be running
+            // ib a device with a smaller screen. Start DetailActivity, passing
+            // the ID of the workout
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+            startActivity(intent);
+        }
+
     }
 }
